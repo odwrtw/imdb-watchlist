15f5481eb5d4452e604416c3ca8ea0dbb18dcb62
Nicolas Duhamel <nicolas@jombi.fr>
2015-05-06 13:58:14 +0200

    distinc movies and tvshows

--- watchlist.go
+++ watchlist.go
@@ -3,18 +3,41 @@ package watchlist
 import (
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"regexp"
 )
 
-var imdbIDRegexp *regexp.Regexp
-
-func init() {
+var (
 	imdbIDRegexp = regexp.MustCompile("tt\\d{7}")
+	imdbUserRe   = regexp.MustCompile("ur\\d{8}") // DEPRECIATED
+	baseURL      = "http://www.imdb.com/user/"
+)
+
+func unique(strs *[]string) {
+	returnslice := []string{}
+	t := map[string]bool{}
+	for _, s := range *strs {
+		t[s] = true
+	}
+	for s := range t {
+		returnslice = append(returnslice, s)
+	}
+	*strs = returnslice
 }
 
-// GetWatchlistIDs returns the imdb ids included in a public watchlist
-func GetWatchlistIDs(watchlist string) (*[]string, error) {
-	resp, err := http.Get(watchlist)
+func getIds(userid string, filter string) (*[]string, error) {
+	URL, err := url.Parse(baseURL)
+	if err != nil {
+		return nil, err
+	}
+	URL.Path += userid
+	URL.Path += "/watchlist"
+
+	parameters := url.Values{}
+	parameters.Add("title_type", filter)
+	URL.RawQuery = parameters.Encode()
+
+	resp, err := http.Get(URL.String())
 	if err != nil {
 		return nil, err
 	}
@@ -25,17 +48,26 @@ func GetWatchlistIDs(watchlist string) (*[]string, error) {
 		return nil, err
 	}
 
-	ids := []string{}
-	matches := imdbIDRegexp.FindAll(body, -1)
+	matches := imdbIDRegexp.FindAllString(string(body), -1)
 	if matches != nil {
-		movieIDs := map[string]bool{}
-		for _, m := range matches {
-			movieIDs[string(m)] = true
-		}
-		for id := range movieIDs {
-			ids = append(ids, id)
-		}
+		unique(&matches)
+		return &matches, nil
 	}
+	return nil, nil
+}
 
-	return &ids, nil
+// GetMovies from userid return slice of ids
+func GetMovies(userid string) (*[]string, error) {
+	return getIds(userid, "movie")
+}
+
+// GetTvSeries from userid return slice of ids
+func GetTvSeries(userid string) (*[]string, error) {
+	return getIds(userid, "tvSeries")
+}
+
+// GetWatchlistIDs return all imdbid DEPRECIATED
+func GetWatchlistIDs(watchlist string) (*[]string, error) {
+	userid := imdbUserRe.FindString(watchlist)
+	return getIds(userid, "movie,tvSeries")
 }
